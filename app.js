5ecf6b78cc398f80f889a3750e0c087c08129ab3
Daniel Brownsey <danielbrownsey01@gmail.com>
2025-08-22 11:25:11 +0100

    feat: added storage templates for image data

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log("hi world");
+
+//functions!
+//TODO: store image data - assetts folder
+//you can use local sorted or web hosted(unsplash, pixa___)
+
+//TOADD var img1 = document.createElement("img");
+//TOADD img1.src = "http://path/to/image";
+
+const images = [
+  { url: "url1", alt: "altText1" },
+  { url: "url2", alt: "altText2" },
+  { url: "url3", alt: "altText3" },
+  { url: "url4", alt: "altText4" },
+  { url: "url5", alt: "altText5" },
+];
+
+//TODO: create thumbnail images
+
+//TODO: create large images
+
+//TODO: create buttons stored in a box that create the large images (and delete the previous large image)
+
+//for each comment, replace pseudo code with actual code; find/work out/big brain this
+
+//TODO: Create image thumbnails
