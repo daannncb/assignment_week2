3fde660ff72a756b43b96c06156f3f03c59ef069
Daniel Brownsey <danielbrownsey01@gmail.com>
2025-08-25 17:24:09 +0100

    feat: image selection now works; no image loaded at first, image loaded on thumbnail click

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-console.log("hi world");
-
-//functions!
-//TODO: store image data - assetts folder
-//you can use local sorted or web hosted(unsplash, pixa___)
-
-//TOADD var img1 = document.createElement("img");
-//TOADD img1.src = "http://path/to/image";
-
 const images = [
   { src: "./assignment_week2_assets/image1.jpg", alt: "altText1" },
   { src: "./assignment_week2_assets/image2.jpg", alt: "altText2" },
@@ -15,68 +6,54 @@ const images = [
   { src: "./assignment_week2_assets/image5.jpg", alt: "altText5" },
 ];
 
-//TODO: create thumbnail images
-//==================ATTEMPT 2===========================================
-//grab thumbnail container
 const imageThumbnail = document.querySelector("#thumbnail-container");
-//loop thumbnails into container
+
 for (let i = 0; i < images.length; i++) {
   const thumb = document.createElement("img");
   thumb.src = images[i].src;
   thumb.alt = images[i].alt;
   thumb.className = "thumbnail";
+  const imageObj = images[i];
+  thumb.addEventListener("click", function () {
+    generateLargeImage(imageObj);
+  });
   imageThumbnail.appendChild(thumb);
 }
 
-//==================ATTEMPT 1 BELOW=====================================
+const largeImageContainer = document.querySelector("#fullres-container");
 
-// function generateThumbnails() {
-//   const imageThumbnail = document.createElement("img");
-//   document.imgSrc = images.src[0];
-// }
+function generateLargeImage(images) {
+  largeImageContainer.innerHTML = "";
+  let fullres = document.createElement("img");
+  fullres.src = images.src;
+  fullres.alt = images.alt;
+  fullres.className = "fullres";
+  largeImageContainer.appendChild(fullres);
+}
 
-// for (let i = 0; i < images.length; i++) {
-//   generateThumbnails((src = images[i]));
-// }
+// thumb.addEventListener("click", () => generateLargeImage(images[i]));
 
-// images.forEach(generateThumbnails);
-// generateThumbnails.addEventListenter("click", function (click) {
-//   console.log("click");
-// });
+// imageThumbnail.addEventListener("click", generateLargeImage());
 
-// const thumbnailContainer = document.getElementById("thumbnail-container");
+//TODO: create thumbnail images
+//==================ATTEMPT 2===========================================
+//grab thumbnail container
 
-// // generateThumbnails;
+//loop thumbnails into container
 
-// for (let i = 0; i < images.length; i++) {
-//   const imgBox = document.createElement("img");
-//   imgBox.imageContent = src = images[i];
-// }
-
-//========================================================================
-
-// function createThumbnails() {
-//select the DOM element (thumnail-container) to contain thumbnails "getElement"
-//   createThumbnails.;
-// this is repetetive, use A LOOP! --> using length (of the images array) as i for
-// inside the loop: need to:
-// Create image element in HTML
-// Update the src to match to array, to pull data from there
-// The clue here is parameters (? not sure)
-// append the created images to the thumbnail-container
-// give each image a className; for i =1 className = image1
-// give each image an eventListener -> event handler for this listener is the function you write to create large images
-
-// }
 //TODO: create large images
 
-//TODO: create buttons stored in a box that create the large images (and delete the previous large image)
+//TODO: make thumbnails interactive (clickable) to create the large images (and delete the previous large image)
 
 //for each comment, replace pseudo code with actual code; find/work out/big brain this
 
 //TODO: Create image thumbnails
 // this task is the event handler for the thumbnail event(s)
 //TODO:          function createLargeImagesHandler(){}
+
+// const largeImageCreator = document.getElementById("#fullres-container");
+
+//    function createLargeImagesHandler() {}
 
 // delete the current image
 // 1) select large image container
